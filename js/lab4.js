f8133d456600b93c9b70c73cb7ca373b6a97b483
Anene <ATerefe@miu.edu>
2020-04-02 22:35:42 -0500

    lab9

--- js/lab4.js
+++ js/lab4.js
@@ -1,22 +1,20 @@
-/* eslint-disable no-console */
+/* eslint-disable */
 /* Author Anene */
 /* file lab4.js */
 window.onload = function(){
     "use strict";
 
-    document.getElementById("form1btn").onclick = function(){
+    /*document.getElementById("form1").addEventListener("submit",function(event){
+        event.preventDefault();
         console.log("Entered inputs:");
         console.log("email="+document.getElementById("exampleInputEmail1").value);
         console.log("password="+document.getElementById("exampleInputPassword1").value);
         console.log("Url="+document.getElementById("url").value);
         console.log("Checked="+document.getElementById("exampleCheck1").checked);
-        let form = document.getElementById("form1");
-        function handleForm(event) { event.preventDefault(); } 
-        form.addEventListener('submit', handleForm);
-    }
-
-    document.getElementById("form2btn").onclick = function(){
-        // eslint-disable-next-line no-alert
+    });
+    
+    document.getElementById("form2").addEventListener("submit",function(event){
+        event.preventDefault();
         confirm(
             "Entered inputs:\n"+
             "Product Number="+document.getElementById("productNumber").value+"\n"+
@@ -24,6 +22,28 @@ window.onload = function(){
             "Unit Price="+document.getElementById("unitPrice").value+"\n"+
             "Quantity="+document.getElementById("quantity").value+"\n"+
             "Supplier="+document.getElementById("supplier").value+"\n"+
-            "Date Supplied="+document.getElementById("dateSupplied").value+"\n")
-    }
+            "Date Supplied="+document.getElementById("dateSupplied").value+"\n");
+    });*/
+
+    $("#form1").submit(function(event){
+        event.preventDefault();
+        console.log("Entered inputs:");
+        console.log("email="+$("#exampleInputEmail1").val());
+        console.log("password="+$("#exampleInputPassword1").val());
+        console.log("Url="+$("#url").val());
+        console.log("Checked="+$("#exampleCheck1").prop("checked"));
+    });
+    
+    $("#form2").submit(function(event){
+        event.preventDefault();
+        $("#inputdata").html(
+            "Entered inputs:<br>"+
+            "Product Number="+$("#productNumber").val()+"<br>"+
+            "Name="+$("#name").val()+"<br>"+
+            "Unit Price="+$("#unitPrice").val()+"<br>"+
+            "Quantity="+$("#quantity").val()+"<br>"+
+            "Supplier="+$("#supplier").val()+"<br>"+
+            "Date Supplied="+$("#dateSupplied").val()+"<br>");
+    });
+
 }
---
+++ js/lab9.js
@@ -0,0 +1,62 @@
+{
+    //Question 1
+    console.log("---------------Q1 Answer------------------------");
+    const person={
+        name:"",
+        dateOfBirth:"",
+        getName:()=>this.name,
+        setName:(newName)=>this.name=newName
+    }
+    const john=Object.create(person);
+    john.setDateOfBirth=(dob)=>this.dateOfBirth=dob;
+    john.getDateOfBirth=()=>{
+            const dob=new Date(this.dateOfBirth);
+        		return (dob.getFullYear()+"-"+(dob.getMonth()+1)+"-"+dob.getDate());
+    }
+    john.setName("John");
+    john.setDateOfBirth(new Date("December 10 1998"));
+    console.log("The person's name is "+john.getName());
+    console.log(john.getName()+ " was born "+john.getDateOfBirth());
+
+    //Question 2
+    console.log("---------------Q2 Answer------------------------");
+    const employee=Object.create(person);
+    employee.salary=0;
+    employee.setSalary=(sal)=>employee.salary=sal;
+    employee.hireDate=new Date();
+    employee.doJob=(jobTitle)=>employee.getName()+" is a "+jobTitle+" who earns $"+employee.salary;
+	const annaemp=Object.create(employee);
+    annaemp.setName("Anna");
+    annaemp.setSalary(249995.50);
+    console.log(annaemp.doJob("Programmer"));
+    
+}
+
+//Question 3
+{
+    //Question 1
+    console.log("---------------Q3 Answer------------------------");
+    function Person(){
+        this.name="";
+        this.dateOfBirth="";
+        this.getName=()=>name;
+        this.setName=(newName)=>name=newName;
+    }
+    const john=new Person();
+    Person.prototype.setDateOfBirth=(dob)=>this.dateOfBirth=dob;
+    Person.prototype.getDateOfBirth=()=>{
+            const dob=new Date(this.dateOfBirth);
+        		return (dob.getFullYear()+"-"+(dob.getMonth()+1)+"-"+dob.getDate());
+    }
+    Person.prototype.toString=()=>{
+    	const dob=new Date(this.dateOfBirth);
+    	return"{Name:"+this.name+", DateOfBirth:"+dob.getFullYear()+"-"+(dob.getMonth()+1)+"-"+dob.getDate()+"}"};
+    john.setName("John");
+    john.setDateOfBirth(new Date("December 10 1998"));
+    console.log(john.toString());
+    
+    const peter=new Person();
+    peter.setName("Peter");
+    peter.setDateOfBirth(new Date("November 10 1985"));
+    console.log(peter.toString());   
+}
